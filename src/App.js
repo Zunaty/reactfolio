87569a91a2f1c1c6eda8d7d16c929cb86d6de754
Victor Perez <zunaty@gmail.com>
2021-10-27 18:24:25 -0600

    Setting up tailwind, and initial components setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 import "tailwindcss/tailwind.css";
+
+import Nav from './components/Nav';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <Nav></Nav>
       </header>
+      
+      <main>
+        <About></About>
+        <Projects></Projects>
+        <Contact></Contact>
+      </main>
+
+      <footer>
+        
+      </footer>
     </div>
   );
 }
---
+++ src/components/Nav/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Nav() {
+    return (
+        <div>
+            <p className="text-9xl text-red-500">Hello</p>
+        </div>
+    );
+};
+
+export default Nav;
