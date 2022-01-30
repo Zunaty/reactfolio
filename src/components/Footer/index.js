7f8b0859a90375be27d30c907665f168935a542e
Victor Perez <zunaty@gmail.com>
2022-01-29 19:58:11 -0700

    Resume link update

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,7 +9,7 @@ function Footer() {
                 <p>Check out these links for more information:</p>
                 <a href="https://github.com/Zunaty" target="_blank" rel="noreferrer" className="hover:text-gray-800 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">GitHub</a>
                 <a href="https://www.linkedin.com/in/victorlperez/" target="_blank" rel="noreferrer" className="hover:text-gray-800 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">LinkedIn</a>
-                <a href="https://docs.google.com/document/d/1HKink6_NjinFAKIUpzhYKVrWZ8Af0Y9yTi8AI_bGc1A/edit?usp=sharing" target="_blank" rel="noreferrer" className="hover:text-gray-800 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">Resume</a>
+                <a href="https://docs.google.com/document/d/1UsKsOZwaKIe5Dg2pLueD0-d8S9xfY343ub4I1yExn_Y/edit?usp=sharing" target="_blank" rel="noreferrer" className="hover:text-gray-800 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">Resume</a>
             </div>
 
             {/* Credits to React and Tailwind */}
